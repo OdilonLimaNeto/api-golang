01f2d6569af4f40d7fea363956f8cab052efbefd
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-08-27 02:00:00 -0500

    create a new users repository with CRUD operations

--- src/repositories/users-repository.go
+++ src/repositories/users-repository.go
@@ -1,9 +1,10 @@
 package repositories
 
 import (
+	"api/src/database"
 	"api/src/models"
 	"database/sql"
-	"fmt"
+	"log"
 )
 
 // represent the users repository
@@ -17,20 +18,161 @@ func NewUsersRepository(db *sql.DB) *Users {
 }
 
 // insert a new user into the database
-func (repository Users) Create(user models.User) (uint64, error) {
-	stmt, err := repository.db.Prepare("INSERT INTO users (name, nick, email, password) VALUES ($1, $2, $3, $4) RETURNING id")
-	fmt.Println("ERRO AQUI --> ", err)
-	if err != nil {
-		fmt.Println("ERRO AQUI --> ", err)
-		return 0, err
-	}
-	defer stmt.Close()
-
-	var id uint64
-	err = stmt.QueryRow(user.Name, user.Email, user.Password).Scan(&id)
+func Create(user models.User) (id uint64, err error) {
+	var (
+		name     = user.Name
+		nick     = user.Nick
+		email    = user.Email
+		password = user.Password
+	)
+	connection, err := database.OpenConnectionDATABASE()
 	if err != nil {
 		return 0, err
 	}
+	defer connection.Close()
 
-	return id, nil
+	SQL := `
+	INSERT INTO users (name, nick, email, password) 
+	VALUES ($1, $2, $3, $4)
+	RETURNING id`
+
+	err = connection.QueryRow(SQL, name, nick, email, password).Scan(&id)
+
+	return id, err
+}
+
+func Get(id int64) (user models.User, err error) {
+	var (
+		name      = user.Name
+		nick      = user.Nick
+		email     = user.Email
+		password  = user.Password
+		createdAt = user.CreatedAt
+		updatedAt = user.UpdatedAt
+		deletedAt = user.DeletedAt
+	)
+	connection, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		return user, err
+	}
+	defer connection.Close()
+
+	SQL := `
+	SELECT 
+		id, 
+		name, 
+		nick, 
+		email, 
+		password, 
+		created_at, 
+		updated_at, 
+		deleted_at 
+	FROM 
+		users 
+	WHERE 
+		id = $1
+	`
+	err = connection.QueryRow(SQL, id).Scan(&user.ID, &name, &nick, &email, &password, &createdAt, &updatedAt, &deletedAt)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func GetAll() (users []models.User, err error) {
+	connection, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection: %v", err)
+		return nil, err
+	}
+	defer connection.Close()
+
+	SQL := `
+	SELECT 
+		id, 
+		name, 
+		nick, 
+		email, 
+		password, 
+		created_at, 
+		updated_at, 
+		deleted_at 
+	FROM 
+		users
+	`
+	rows, err := connection.Query(SQL)
+	if err != nil {
+		log.Printf("Error on get all users: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+		err := rows.Scan(&user.ID, &user.Name, &user.Nick, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
+		if err != nil {
+			log.Printf("Error to get rows: %v", err)
+			continue
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
+
+func Update(id int64, user models.User) (int64, error) {
+	var (
+		name      = user.Name
+		nick      = user.Nick
+		email     = user.Email
+		password  = user.Password
+		createdAt = user.CreatedAt
+		updatedAt = user.UpdatedAt
+		deletedAt = user.DeletedAt
+	)
+	connection, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		return 0, err
+	}
+	defer connection.Close()
+
+	SQL := `
+	UPDATE 
+		users 
+	SET 
+		name = $1, 
+		nick = $2, 
+		email = $3, 
+		password = $4, 
+		created_at = $5,
+		updated_at = $6,
+		deleted_at = $7 
+	WHERE 
+		id = $8
+	`
+	response, err := connection.Exec(SQL, name, nick, email, password, createdAt, updatedAt, deletedAt, id)
+	if err != nil {
+		return 0, err
+	}
+	return response.RowsAffected()
+}
+
+func Delete(id int64) (int64, error) {
+	connection, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection: %v", err)
+		return 0, err
+	}
+	defer connection.Close()
+
+	SQL := `
+	DELETE FROM users 
+	WHERE 
+		id = $1
+	`
+	response, err := connection.Exec(SQL, id)
+	if err != nil {
+		log.Printf("Error on delete user: %v", err)
+		return 0, err
+	}
+	return response.RowsAffected()
 }
