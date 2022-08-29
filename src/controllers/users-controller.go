abb0a2db614ff903a8a8cb96cdb58c99630c7746
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-08-29 01:21:20 -0500

    refactor code strategy to execute CRUD operations on controller layer

--- src/controllers/users-controller.go
+++ src/controllers/users-controller.go
@@ -6,51 +6,199 @@ import (
 	"api/src/repositories"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-// Create is the handler for POST /users
 func Create(w http.ResponseWriter, r *http.Request) {
-	request, err := ioutil.ReadAll(r.Body)
+	var request struct {
+		Name     string `json:"name"`
+		Nick     string `json:"nick"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Parse the request body into the struct
+	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error decoding request body: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
-	var user models.User
-	if err = json.Unmarshal(request, &user); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+
+	// Validate the request body
+	if request.Name == "" || request.Nick == "" || request.Email == "" || request.Password == "" {
+		log.Printf("Error validating request body: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
-	db, err := database.ConnectDB()
+
+	database, err := database.OpenConnectionDATABASE()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error opening connection to database: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
+	defer database.Close()
 
-	repository := repositories.NewUsersRepository(db)
+	repository := repositories.NewUsersRepository(database)
 
-	id, err := repository.Create(user)
+	id, err := repository.Create(models.User{
+		Name:     request.Name,
+		Nick:     request.Nick,
+		Email:    request.Email,
+		Password: request.Password,
+	})
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error creating user: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Println("id --> ", id)
-
-	w.Write([]byte(fmt.Sprintf("Inserted ID %d", id)))
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("{\"New user ID\": %d}", id)))
 }
 
-// List is the handler for /users
 func List(w http.ResponseWriter, r *http.Request) {
+
+	database, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection to database: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer database.Close()
+
+	repository := repositories.NewUsersRepository(database)
+
+	users, err := repository.GetAll()
+	if err != nil {
+		log.Printf("Error getting users: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 
-// Get is the handler for /users/{id}
 func Get(w http.ResponseWriter, r *http.Request) {
+	var err error
+	id := mux.Vars(r)["id"]
+	if id == "" {
+		log.Printf("Error getting ID: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	convertedID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		log.Printf("Error converting ID: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	database, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection to database: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer database.Close()
+
+	repository := repositories.NewUsersRepository(database)
+
+	user, err := repository.Get(convertedID)
+	if err != nil {
+		log.Printf("Error getting user: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
 
-// Update is the handler for PUT /users/{id}
 func Update(w http.ResponseWriter, r *http.Request) {
+	var request struct {
+		Name     string `json:"name"`
+		Nick     string `json:"nick"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	params := mux.Vars(r)
+	convertedID, err := strconv.ParseInt(params["id"], 10, 64)
+	if err != nil {
+		log.Printf("Error converting id: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Printf("Error decoding request body: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	if request.Name == "" || request.Nick == "" || request.Email == "" || request.Password == "" {
+		log.Printf("Error validating request body: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	database, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection to database: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer database.Close()
+
+	repository := repositories.NewUsersRepository(database)
+
+	_, err = repository.Update(convertedID, models.User{
+		Name:     request.Name,
+		Nick:     request.Nick,
+		Email:    request.Email,
+		Password: request.Password,
+	})
+	if err != nil {
+		log.Printf("Error updating user: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("{\"Updated user ID\": %d}", convertedID)))
 }
 
-// Delete is the handler for DELETE /users/{id}
 func Delete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	convertedID, err := strconv.ParseInt(params["id"], 10, 64)
+	if err != nil {
+		log.Printf("Error converting id: %s", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	database, err := database.OpenConnectionDATABASE()
+	if err != nil {
+		log.Printf("Error opening connection to database: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer database.Close()
+
+	repository := repositories.NewUsersRepository(database)
+
+	_, err = repository.Delete(convertedID)
+	if err != nil {
+		log.Printf("Error deleting user: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("{\"Deleted user ID\": %d}", convertedID)))
 }
